2759d039a315cb73f402cea0f4dad45375c25d5d
Daria Mishalkina <dashaio251@gmail.com>
2023-06-26 16:34:52 +0200

    sign up link style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {FunctionComponent, ReactElement} from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { ROUTES } from "components/Naviagation/constants/constants";
 import {RouteItem} from "components/Naviagation/types/types";
 import './App.scss';
